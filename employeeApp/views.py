3901786e5df8890c165516b36f37723f4a7fe6be
Pramod Pant <44371066+PantPramod@users.noreply.github.com>
2022-02-20 10:06:31 +0530

    Update views.py

--- employeeApp/views.py
+++ employeeApp/views.py
@@ -8,7 +8,7 @@ from employeeApp.models import Employee
 
 
 class EmpClassView(RetrieveUpdateDestroyAPIView):
-    queryset = Employee.objects.all()
+    queryset = Employee.objects.all().order_by('id')
     serializer_class = EmployeeSerializer
 
 
